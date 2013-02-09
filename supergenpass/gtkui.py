49c5cee4b7f7bedb31c3098bb8e3cd20d25cfb23
Vianney le Clément de Saint-Marcq <vleclement@gmail.com>
2013-02-09 17:15:05 +0100

    gtkui: Add visual hash for master password
    The drawing algorithm is ported from
    http://staticfree.info/projects/sgp/ to produce
    the same results as the Android app.

--- supergenpass/gtkui.py
+++ supergenpass/gtkui.py
@@ -14,6 +14,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import math
 import os.path
 import hashlib
 from gi.repository import Gtk, Gdk, GLib
@@ -43,10 +44,13 @@ class GtkUI:
         builder.add_from_file(os.path.join(data_dir, 'main.ui'))
         # initialize members
         self.password = ''
+        self.masterhash = None
+        self.shapes = []
         self.window = builder.get_object('main')
         self.f_domain = builder.get_object('domain')
         self.f_master = builder.get_object('master')
         self.f_confirm = builder.get_object('confirm')
+        self.f_visualhash = builder.get_object('visualhash')
         self.f_method = builder.get_object('method')
         self.f_password = builder.get_object('password')
         self.f_show_password = builder.get_object('show_password')
@@ -112,6 +116,15 @@ class GtkUI:
         domain = self.f_domain.get_text()
         master = self.f_master.get_text()
         confirm = self.f_confirm.get_text()
+        # Compute visual hash
+        if master:
+            masterhash = hashlib.sha1(master.encode()).digest()
+        else:
+            masterhash = None
+        if masterhash != self.masterhash:
+            self.masterhash = masterhash
+            self.f_visualhash.queue_draw()
+        # Check confirmed password
         if not confirm:
             confirm = master
         ctx = self.f_confirm.get_style_context()
@@ -119,6 +132,7 @@ class GtkUI:
             ctx.add_class('invalid')
         else:
             ctx.remove_class('invalid')
+        # Generate password
         if domain and master and master == confirm:
             master = master + self.f_salt.get_text()
             if self.method == 0:  # Password
@@ -151,4 +165,113 @@ class GtkUI:
             clipboard.set_text("", 0)
             clipboard.store()
         Gtk.main_quit()
+        return False
+
+    def on_draw_visualhash(self, widget, cr):
+        if not self.masterhash:
+            return False
+
+        radius = 8  # radius of the shapes defined below
+        spacing = 2  # spacing between shapes
+        shapewidth = radius * 2 + spacing  # width of a shape
+        nwidth = 4  # width of canvas in shapes
+
+        # Generate shapes based on radius 8
+        if not self.shapes:
+            # Circle
+            cr.arc(0, 0, radius, 0, 2 * math.pi)
+            self.shapes.append(cr.copy_path())
+            cr.new_path()
+            # Square
+            cr.rectangle(-radius, -radius, radius, radius)
+            self.shapes.append(cr.copy_path())
+            cr.new_path()
+            # Star
+            cr.move_to(0, -8.475681)
+            cr.line_to(1.893601, -2.597389)
+            cr.line_to(8.069343, -2.612960)
+            cr.line_to(3.063910, 1.004453)
+            cr.line_to(4.987128, 6.873122)
+            cr.line_to(0, 3.230514)
+            cr.line_to(-4.987129, 6.873122)
+            cr.rel_line_to(1.923218, -5.868669)
+            cr.rel_line_to(-5.005433, -3.617414)
+            cr.rel_line_to(6.175743, 0.015571)
+            cr.close_path()
+            self.shapes.append(cr.copy_path())
+            cr.new_path()
+            # Triangle
+            cr.move_to(-radius, radius)
+            cr.line_to(radius, radius)
+            cr.line_to(0, -radius)
+            cr.close_path()
+            self.shapes.append(cr.copy_path())
+            cr.new_path()
+            # Plus
+            cr.move_to(2.084458, -2.117061)
+            cr.rel_line_to(5.865234, 0)
+            cr.rel_line_to(0, 4.296875)
+            cr.rel_line_to(-5.865234, 0)
+            cr.rel_line_to(0, 5.865234)
+            cr.rel_line_to(-4.296875, 0)
+            cr.rel_line_to(0, -5.865234)
+            cr.rel_line_to(-5.865234, 0)
+            cr.rel_line_to(0, -4.296875)
+            cr.rel_line_to(5.865234, 0)
+            cr.rel_line_to(0, -5.875977)
+            cr.rel_line_to(4.296875, 0)
+            cr.close_path()
+            self.shapes.append(cr.copy_path())
+            cr.new_path()
+            # X
+            cr.move_to(3.723963, 0.060475)
+            cr.line_to(8.083338, 4.419850)
+            cr.line_to(4.438807, 8.064382)
+            cr.line_to(0.079432, 3.705007)
+            cr.line_to(-4.279943, 8.064382)
+            cr.line_to(-7.924475, 4.419850)
+            cr.rel_line_to(4.359375, -4.359375)
+            cr.rel_line_to(-4.359375, -4.359375)
+            cr.rel_line_to(3.644531, -3.644531)
+            cr.rel_line_to(4.359375, 4.359375)
+            cr.rel_line_to(4.359375, -4.371094)
+            cr.rel_line_to(3.644531, 3.644531)
+            cr.close_path()
+            self.shapes.append(cr.copy_path())
+            cr.new_path()
+            # Diamond
+            cr.move_to(0, -radius)
+            cr.line_to(radius, 0)
+            cr.line_to(0, radius)
+            cr.line_to(-radius, 0)
+            cr.close_path()
+            self.shapes.append(cr.copy_path())
+            cr.new_path()
+            # Small circle
+            cr.arc(0, 0, radius/2, 0, 2 * math.pi)
+            self.shapes.append(cr.copy_path())
+            cr.new_path()
+
+        # Setup cairo context
+        scale = min(widget.get_allocated_width() / (nwidth * shapewidth),
+                    widget.get_allocated_height() / (nwidth * shapewidth))
+        cr.scale(scale, scale)
+        cr.translate(shapewidth / 2, shapewidth / 2)
+
+        # Draw the hash
+        it = iter(self.masterhash)
+        for dat1, dat2 in zip(it, it):
+            dat = dat1 | dat2 << 8
+            symbol = dat & 0x7
+            x = (dat >> 3 & 0x7) / 7 * (nwidth - 1) * shapewidth
+            y = (dat >> 6 & 0x7) / 7 * (nwidth - 1) * shapewidth
+            r = (dat >> 9 & 0x3) / 3
+            g = (dat >> 11 & 0x3) / 3
+            b = (dat >> 13 & 0x3) / 3
+            cr.set_source_rgb(r, g, b)
+            cr.save()
+            cr.translate(x, y)
+            cr.append_path(self.shapes[symbol])
+            cr.fill()
+            cr.restore()
         return False
